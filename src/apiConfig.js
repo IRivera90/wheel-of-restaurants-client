3d33cd6a75dec0c016953efbfbc9b2677f428b35
IRivera90 <isacsac90@gmail.com>
2020-03-10 12:41:50 -0400

    Corrected typo in production url
    Removed '/' from end of URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://immense-meadow-91839.herokuapp.com/',
+  production: 'https://immense-meadow-91839.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
