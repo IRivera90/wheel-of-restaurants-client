280112b768358cdfc9f026f126d711166afba5e5
IRivera90 <isacsac90@gmail.com>
2020-03-10 11:42:43 -0400

    Made changes to Home button
    Removed the Home link and redirected it in the header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,22 +18,23 @@ const unauthenticatedOptions = (
   </Fragment>
 )
 
-const alwaysOptions = (
-  <Fragment>
-    <Nav.Link to="/">Home</Nav.Link>
-  </Fragment>
-)
+// const alwaysOptions = (
+//   <Fragment>
+//     <Nav.Link href="/">Home</Nav.Link>
+//   </Fragment>
+// )
 
+// { alwaysOptions } after user && <span...
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      react-auth-template
+      Home
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
         { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
-        { alwaysOptions }
+
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
     </Navbar.Collapse>
